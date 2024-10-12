eda4a3f49744b92d1d4f6ad67614038ecbcd6189
ASM Tamzid <atkthegreat9999@gmail.com>
2024-10-12 22:15:36 +0600

    addead pakegs

---
+++ src/main/java/com/system/banking/mapper/AccountMapper.java
@@ -0,0 +1,25 @@
+package com.system.banking.mapper;
+
+import com.system.banking.dto.AccountDto;
+import com.system.banking.entity.Account;
+
+public class AccountMapper {
+    public static Account mapToAccount(AccountDto accountDto){
+        Account account = new Account(
+                accountDto.getId(),
+                accountDto.getAccountHolderName(),
+                accountDto.getBalance());
+
+        return account;
+    }
+
+    public static AccountDto mapToAccountDto(Account account){
+        AccountDto accountDto = new AccountDto(
+                account.getId(),
+                account.getAccountHolderName(),
+                account.getBalance()
+        );
+
+        return accountDto;
+    }
+}
