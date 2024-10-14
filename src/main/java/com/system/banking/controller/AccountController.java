bab6df1ea3bba4f79b7cc03feaf7804366797157
ASM Tamzid <atkthegreat9999@gmail.com>
2024-10-14 22:56:03 +0600

    add account done

--- src/main/java/com/system/banking/controller/AccountController.java
+++ src/main/java/com/system/banking/controller/AccountController.java
@@ -5,17 +5,24 @@ import com.system.banking.service.AccountService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
 
 @RestController
 @RequestMapping("/api/accounts")
 public class AccountController {
+    private final AccountService accountService;
+
+    // Constructor injection is preferred for better testability and clarity
+
     @Autowired
-    private AccountService accountService;
+    public AccountController(AccountService accountService) {
+        this.accountService = accountService;
+    }
 
-
-    public ResponseEntity<AccountDto> addAccount(AccountDto accountDto){
-        return new ResponseEntity<>(accountService.createAccount(accountDto), HttpStatus.CREATED);
+    @PostMapping
+    public ResponseEntity<AccountDto> addAccount(@RequestBody AccountDto accountDto){
+        AccountDto createdAccount = accountService.createAccount(accountDto);
+        return new ResponseEntity<>(createdAccount, HttpStatus.CREATED);
     }
 }
